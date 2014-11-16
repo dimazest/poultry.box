fffee4b35cc1ff77a3d6a86e51668c3b6f1b0df1
Dmitrijs Milajevs <dimazest@gmail.com>
2014-11-16 12:29:28 +0000

    Reverting sklExperiment.py

--- sklExperiment.py
+++ sklExperiment.py
@@ -4,20 +4,20 @@ An example script for training & testing a classifier using LIBLINEAR
 Copyright Matthew Purver 2014
 """
 
-from __future__ import print_function
-
+from collections import deque
+from itertools import chain
 from unicodecsv import DictReader
 
 import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC, LinearSVC
 from sklearn.cross_validation import cross_val_score
-
+from sklearn.svm.libsvm import cross_validation
 
 START_SYMBOL = '__START__'
 END_SYMBOL = '__END__'
-
 
 def getTweets(csvfile='data.csv'):
     """
@@ -35,7 +35,7 @@ def getTweets(csvfile='data.csv'):
             if labelKey in line and line[labelKey] and line[labelKey].lstrip('-').isdigit():
                 yield line[labelKey], line[textKey]
 
-
+                
 def getVectors(labelTextPairs, labels=None):
     """
     Turn iterable label-text pair sequences into sklearn feature vector and label arrays
@@ -43,7 +43,7 @@ def getVectors(labelTextPairs, labels=None):
     y = []
     x = []
     y_values = {}
-    for label, text in labelTextPairs:
+    for (label,text) in labelTextPairs:
         if labels and float(label) not in labels:
             continue
         y.append(float(label))
@@ -52,16 +52,16 @@ def getVectors(labelTextPairs, labels=None):
             y_values[float(label)] += 1
         except KeyError:
             y_values[float(label)] = 1
-    print("read %s datapoints, with these labels: %s" % (len(y), y_values))
-    vectorizer = CountVectorizer(min_df=1,
-                                 ngram_range=(1, 3),
-                                 token_pattern=u'(?u)\\b\\w+\\b',
+    print "read %s datapoints, with these labels: %s" % (len(y),y_values)
+    vectorizer = CountVectorizer(min_df=1, 
+                                 ngram_range=(1,3), 
+                                 token_pattern=u'(?u)\\b\\w+\\b', 
                                  binary=False)
     X = vectorizer.fit_transform(x)
-    # print vectorizer.get_feature_names()
+    #print vectorizer.get_feature_names()
     X = TfidfTransformer(use_idf=False).fit_transform(X)
     # X = MinMaxScaler().fit_transform(X)
-    # print X.toarray()
+    #print X.toarray()
     return X, y
 
 
@@ -70,13 +70,13 @@ def crossValidate(X, y, nfolds=10, cost=None):
     Run a k-fold (default 10) cross-validation using a linear SVM
     """
     svm = LinearSVC(C=cost) if cost else LinearSVC()
-    # svm.fit(X, y)
+    #svm.fit(X, y)
     acc = cross_val_score(svm, X, y, cv=5)
-    print("Cross-validation accuracy %s: %s" % (np.mean(acc), acc))
+    print "Cross-validation accuracy %s: %s" % (np.mean(acc),acc)
 
     # compare to LibSVM
     acc = cross_val_score(SVC(kernel='rbf', C=cost) if cost else SVC(kernel='linear'), X, y, cv=5)
-    print("Cross-validation accuracy %s: %s" % (np.mean(acc), acc))
+    print "Cross-validation accuracy %s: %s" % (np.mean(acc),acc)
 
     # # there is a low-level LibSVM x-val function too, is it faster?
     # acc = cross_validation(X.toarray(), np.array(y), 5)
@@ -87,5 +87,5 @@ def crossValidate(X, y, nfolds=10, cost=None):
 
 if __name__ == "__main__":
 
-    X, y = getVectors(getTweets(), labels=[-1, 1])
+    X, y = getVectors(getTweets(), labels=[-1,1])
     crossValidate(X, y, cost=3)
